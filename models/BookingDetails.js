c07eb6a060a10cb1d23d164de7d0af603b02e1ef
Rishav <suranarishav08@gmail.com>
2024-01-29 22:23:17 +0530

    Fix: change in booking controller

--- models/BookingDetails.js
+++ models/BookingDetails.js
@@ -1,28 +1,28 @@
-const mongoose = require('mongoose');
-const {Schema} = mongoose
+const mongoose = require("mongoose");
+const { Schema } = mongoose;
 
 const BookingSchema = new Schema({
-    BookingId: {
-        type: String, 
-        require: true
-    },
-    UserId: {
-        type: String,
-        require: true
-    },
-    CoachId: {
-        type: String,
-        require: true
-    },
-    AppointmentDate: {
-        type: Date,
-        require: true
-    },
-    Slot: {
-        type: String,
-        require: true
-    }
-})
+  bookingId: {
+    type: String,
+    require: true,
+  },
+  userId: {
+    type: String,
+    require: true,
+  },
+  coachId: {
+    type: String,
+    require: true,
+  },
+  appointmentDate: {
+    type: Date,
+    require: true,
+  },
+  slot: {
+    type: String,
+    require: true,
+  },
+});
 
-const Booking = mongoose.model('booking', BookingSchema)
-module.exports = Booking
+const Booking = mongoose.model("booking", BookingSchema);
+module.exports = Booking;
